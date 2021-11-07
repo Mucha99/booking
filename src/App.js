4924f02b63fdd705e8d4781d7bf3df566a417602
Bartek Mucha <bartekmucha43@gmail.com>
2021-11-07 13:08:31 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/layout/Header/Header";
+import Menu from "./components/layout/Menu/Menu";
+import Hotels from "./components/views/Hotels/Hotels";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Menu />
+      <Hotels />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/views/Hotels/Hotels.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hotel from '../Hotel/Hotel';
+
+class Hotels extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Hotels</h3>
+        <Hotel />
+      </div>
+    );
+  }
+}
+
+export default Hotels;
